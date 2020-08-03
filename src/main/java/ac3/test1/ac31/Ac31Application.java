3004473b2288f2de9a14fa16b8ea86fd5a959703
kevin ard <muhammad.ardela@dana.id>
2020-08-03 17:35:24 +0700

    Initial commit

---
+++ src/main/java/ac3/test1/ac31/Ac31Application.java
@@ -0,0 +1,13 @@
+package ac3.test1.ac31;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ac31Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ac31Application.class, args);
+	}
+
+}
---
+++ src/test/java/ac3/test1/ac31/Ac31ApplicationTests.java
@@ -0,0 +1,13 @@
+package ac3.test1.ac31;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ac31ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
